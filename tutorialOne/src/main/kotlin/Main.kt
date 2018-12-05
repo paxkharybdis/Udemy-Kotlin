9f101fb676a250a8dd204a55618f86614608d282
Nathan Thomas <n8shome@gmail.com>
2018-12-05 09:45:14 -0600

    Started Section 5: The Button Counter App

---
+++ tutorialOne/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    val tim = Player("Tim")
+    tim.show()
+
+    val louise = Player("Louise", 5)
+    louise.show()
+
+    val charlie = Player("Charlie", 4, 8, 0)
+    charlie.show()
+
+    val denise = Player("Denise", 2, 5, 1000)
+    denise.show()
+
+}

2fcb871c24f36ab1f225bddd989a7ed116c4c7b2
paxkharybdis <n8shome@gmail.com>
2018-12-14 11:51:56 -0600

    Update files.

--- SplashCalc/app/src/main/java/org/nathanthomas/splashcalc/SplashActivity.kt
+++ SplashCalc/app/src/main/java/org/nathanthomas/splashcalc/SplashActivity.kt
@@ -9,13 +9,15 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        overridePendingTransition(R.anim.fade_in,R.anim.fade_out)
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
         setContentView(R.layout.activity_splash)
 
 
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-        }, 10000)
+        }, 8000)
+
+
     }
 }
